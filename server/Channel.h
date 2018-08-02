1963f39fed2ee8a765f026c6ce95e5fc34505d50
Anna Plust <anna.plust@tieto.com>
2018-08-02 15:01:02 +0200

    seding message

--- server/Channel.h
+++ server/Channel.h
@@ -3,6 +3,7 @@
 
 #include <netinet/in.h>
 #include <sys/epoll.h>
+#include <map>
 
 class Channel
 {
@@ -11,10 +12,14 @@ class Channel
         ~Channel();
         void handle_connections();
 
+    protected:
+        virtual void send_message(int socket_fd) = 0;
+
     private:
         void set_socket_non_blocking(int socket_fd);
         void accept_new_connection();
         void read_incoming_data(int event_fd);
+        void send_data(int socket_fd, const char message[1024]);
 
         int port;
         int socket_fd, epoll_fd;
@@ -24,6 +29,8 @@ class Channel
         struct epoll_event *events;
 
         const int max_number_of_events;
+
+        std::map<int, int> clients_fds;
 };
 
 #endif
