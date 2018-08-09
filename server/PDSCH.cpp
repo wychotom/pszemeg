32399ac24309a9a0a8664e1c917f4354c68d7890
Tomasz Wychocki <tomasz.wychocki@tieto.com>
2018-08-09 12:19:44 +0200

    rrc connection response

--- server/PDSCH.cpp
+++ server/PDSCH.cpp
@@ -30,21 +30,44 @@ void PDSCH::send_random_access_response(UE &ue)
     std::cout << "\033[1;33m[PDSCH]\033[0m RAR sent to " << ue.RA_RNTI << std::endl;
 }
 
+void PDSCH::send_rrc_connection_response(UE &ue)
+{
+    struct RRC_CONN_SETUP rcr_message;
+
+    rcr_message.C_RNTI = ue.C_RNTI;
+    rcr_message.dl_am_rlc = 0;
+    rcr_message.phr_config = 1;
+    rcr_message.srb_identity = 65536;
+    rcr_message.ul_am_rlc = 1;
+    rcr_message.ul_sch_config = 2;
+    rcr_message.uplink_power_control = 3;
+
+    send_message((void*) &rcr_message, sizeof(struct RRC_CONN_SETUP));
+
+    std::cout << "\033[1;33m[PDSCH]\033[0m RCR sent to " << ue.C_RNTI << std::endl;
+}
+
 void PDSCH::handle_queue(std::vector<UE*> &ue_vector)
 {
     UE *ue;
     Action_to_perform rar_action = Action_to_perform::random_access_response;
+    Action_to_perform rcr_action = Action_to_perform::rrc_connection_response;
 
     while((ue = get_next_ue(ue_vector, rar_action)) != nullptr)
     {
         ue->C_RNTI = 100 * ue->RA_RNTI;
         send_random_access_response(*ue);
     }
+
+    while((ue = get_next_ue(ue_vector, rcr_action)) != nullptr)
+    {
+        send_rrc_connection_response(*ue);
+    }
 }
 
-UE* PDSCH::get_next_ue(const std::vector<UE*> &ue_vector, Action_to_perform action)
+UE* PDSCH::get_next_ue(std::vector<UE*> &ue_vector, Action_to_perform action)
 {
-    auto it = std::find_if(ue_vector.cbegin(), ue_vector.cend(), [&action] (UE* client)
+    auto it = std::find_if(ue_vector.begin(), ue_vector.end(), [&action] (UE* client)
     {
         return client->get_flag() == action;
     });
@@ -54,5 +77,9 @@ UE* PDSCH::get_next_ue(const std::vector<UE*> &ue_vector, Action_to_perform acti
         return nullptr;
     }
 
-    return *it;
+    UE *ue = *it;
+
+    ue_vector.erase(it);
+
+    return ue;
 }
--- server/PDSCH.h
+++ server/PDSCH.h
@@ -13,7 +13,8 @@ class PDSCH : public Uplink_channel
 
     private:
         void send_random_access_response(UE &ue);
-        UE* get_next_ue(const std::vector<UE*> &ue_vector, Action_to_perform action);
+        void send_rrc_connection_response(UE &ue);
+        UE* get_next_ue(std::vector<UE*> &ue_vector, Action_to_perform action);
 };
 
 
